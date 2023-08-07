71831843e6ea8bf34b3326edf779fafadb125bb2
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-07 13:41:41 -0300

    req 1,2 ok

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,13 +1,30 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )  # lança um ReadTimeout se demorar mais de 3 seg
+        time.sleep(1)
+        response.raise_for_status()
+        return response.text
+    except requests.HTTPError:
+        print("HTTP ERROR")
+        return None
+    except requests.ReadTimeout:
+        print("READ TIMEOUT")
+        return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(html_content)
+    updates = selector.css(".cs-overlay-link::attr(href)").getall()
+    return updates
 
 
 # Requisito 3
