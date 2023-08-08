8520edb33acd74ed64bccb9e9f8588e2d5105d0d
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-08 19:40:20 -0300

    req 7 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,20 @@
+import re
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_filtered_list = []
+    case_insensitive_title = re.compile(title, re.IGNORECASE)
+    for news in db.news.find(
+        {"title": {"$regex": case_insensitive_title}},
+        {"_id": 0, "title": 1, "url": 1},
+    ):
+        for key, value in news.items():
+            if key == "title":
+                new_tuple = value, news["url"]
+                news_filtered_list.append(new_tuple)
+    return news_filtered_list
 
 
 # Requisito 8
