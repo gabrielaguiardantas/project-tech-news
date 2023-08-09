887fc97f329417b25e4cfedf9e9758d8e0917521
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-09 15:40:09 -0300

    projeto concluído

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,18 @@
+from tech_news.database import db
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    top_5_categories_list = []
+    categories_list = []
+    for category in db.news.find({}, {"_id": 0, "category": 1}):
+        categories_list.append((category["category"]))
+    categories_counter = Counter(categories_list)
+    categories_top_5 = sorted(
+        categories_counter.most_common(5), key=lambda item: (-item[1], item[0])
+    )
+    top_5_categories_list = [
+        k for k, v in categories_top_5 if categories_top_5.index((k, v)) < 5
+    ]
+    return top_5_categories_list
